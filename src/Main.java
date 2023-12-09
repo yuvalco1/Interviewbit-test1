70bb30fbfddf2eac6d43dffebba3c5dee0e4c118
Yuval Cohen <yuvalco1@gmail.com>
2023-12-09 18:31:41 +0200

    add code comments

--- src/Main.java
+++ src/Main.java
@@ -5,44 +5,32 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
-
 public class Main {
-
-
-//  Interviewbit Spiral Order Matrix I Solution - https://www.interviewbit.com/problems/spiral-order-matrix-i/
-//Given a matrix of M * N elements (M rows, N columns), return all elements of the matrix in spiral order.
-//1 <= M, N <= 1000
-//Input Format - The first argument is a matrix A.  Output Format - Return an array of integers representing all elements of the matrix in spiral order.
 
     public static int[] spiralOrder(final int[][] A) {
         ArrayList<Integer> B = new ArrayList<Integer>();
 
-// iterate normally through the matrix
-//        int M = A.length;
-//        int N = A[0].length;
-//        for(int i=0; i < M; i++) {
-//            for (int j=0; j < N; j++) {
-//                B.add(A[i][j]);
-//            }
-//        }
-
-//      iterate through the matrix in spiral order
+        // Define the boundaries of the matrix
         int rowBegin = 0;
         int rowEnd = A.length - 1;
         int columnBegin = 0;
         int columnEnd = A[0].length -1;
 
+        // Loop through the matrix in a spiral order
         while(rowBegin <= rowEnd && columnBegin <= columnEnd) {
+            // Traverse right
             for(int i = columnBegin; i <= columnEnd; i++){
                 B.add(A[rowBegin][i]);
             }
             rowBegin++;
 
+            // Traverse down
             for(int i = rowBegin; i <= rowEnd; i++){
                 B.add(A[i][columnEnd]);
             }
             columnEnd--;
 
+            // Traverse left
             if(rowBegin <= rowEnd){
                 for(int i = columnEnd; i >= columnBegin; i--){
                     B.add(A[rowEnd][i]);
@@ -50,6 +38,7 @@ public class Main {
             }
             rowEnd--;
 
+            // Traverse up
             if(columnBegin <= columnEnd){
                 for(int i = rowEnd; i>= rowBegin; i--){
                     B.add(A[i][columnBegin]);
@@ -58,6 +47,7 @@ public class Main {
             columnBegin++;
         }
 
+        // Convert the ArrayList to an array
         int[] C = new int[B.size()];
         for(int i = 0; i < C.length; i++)
             C[i] = B.get(i);
@@ -65,32 +55,26 @@ public class Main {
         return C;
     }
 
-
-//Interviewbit Max Non Negative SubArray Solution - https://www.interviewbit.com/problems/max-non-negative-subarray/
-//Problem Description : Given an array of integers, A of length N, find out the maximum sum sub-array of non negative numbers from A.
-//The sub-array should be contiguous i.e., a sub-array created by choosing the second and fourth element and skipping the third element is invalid.
-//Maximum sub-array is defined in terms of the sum of the elements in the sub-array. Find and return the required subarray.
-//NOTE: If there is a tie, then compare with segment's length and return segment which has maximum length.
-//If there is still a tie, then return the segment with minimum starting index.
-//Input Format: The first and the only argument of input contains an integer array A, of length N.
-//1 <= N <= 10^5  ; -10^9 <= A[i] <= 10^9
-
-
     public static int[] maxset(int[] A) {
         List<Map<Long, int[]>> setList = new ArrayList<Map<Long, int[]>>();
         int N = A.length;
         int retArr[] = new int[0];
+
+        // Loop through the array to find all non-negative subarrays
         for (int i = 0; i < N; i++) {
             if (A[i] >= 0) {
                 int j = i;
+                // Find the end of the current non-negative subarray
                 while (j < N && A[j] >= 0) {
                     j++;
                 }
+                // Create a subarray and calculate its sum
                 int[] set = Arrays.copyOfRange(A, i, j);
                 long setsum = 0;
                 for (int k = 0; k < set.length; k++) {
                     setsum += set[k];
                 }
+                // Store the subarray and its sum in a map, and add the map to the list
                 int setStartIndex = i;
                 int[] finalset = new int[set.length + 2];
                 finalset[0] = set.length;
@@ -104,6 +88,8 @@ public class Main {
                 i = j;
             }
         }
+
+        // Find the subarray with the maximum sum
         long previousKey= 0;
         for (Map<Long, int[]> map : setList) {
             for (Map.Entry<Long, int[]> entry : map.entrySet()) {
@@ -121,35 +107,32 @@ public class Main {
                         }
                     }
                 }
-
             }
-
         }
+
+        // Return the subarray with the maximum sum
         if (setList.size() == 0) {
             return new int[]{};
         }else {
             return Arrays.copyOfRange(retArr, 2, retArr.length);
         }
-
     }
 
-
-        public static void main(String[] args) {
-
-
+    public static void main(String[] args) {
+        // Define a 2D array
         int[][] arr = { {1, 2, 3},
-                        {4, 5, 6},
-                        {7, 8, 9} };
+                {4, 5, 6},
+                {7, 8, 9} };
 
+        // Call the spiralOrder method and print the result
         int[] arr2;
         arr2 = spiralOrder(arr);
+        System.out.println(Arrays.toString(arr2));
 
-        System.out.println(   Arrays.toString(arr2));
-
-        //int [] aar = {-63216, -75973, -82720, -81169, 15827, -16728, -20384, 89949, 16046, -68666, 60520, 41991, -33258, 68744, 85718, -66825, 58689, -73014, 2918, 33029, 6727, 33754, -8396, 3083, -46865, 15252, -77876, -32675, -82710, 28192, -54747};
+        // Define an array
         int [] aar = {-63216, -75973, -82720, -81169, 15827, -16728, -20384, 89949, 16046, -68666, 60520, 41991, -33258, 68744, 85718, -66825, 58689, -73014, 2918, 33029, 6727, 33754, -8396, 3083, -46865, 15252, -77876, -32675, -82710, 28192, -54747 };
+
+        // Call the maxset method
         int [] aar2 = maxset(aar);
-
-
     }
 }
